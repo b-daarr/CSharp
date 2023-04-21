dafb669719e339ac5ca3015cd2ecc020bb3b3e62
ѠΞẞ <118155606+ALTONIBOT@users.noreply.github.com>
2023-04-21 10:26:02 +0430

    Files uploaded again

---
+++ Console/Book/P-Even/P-Even/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 30; i+=2)
+            {
+                Console.Write("  " + i);
+            }
+
+            Console.Write("\n");
+
+            for (int a = 0; a <= 30; a++)
+            {
+                a++;
+                Console.Write("  " + a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Console/Book/P-Max/P-Max/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour, min, max = 0;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Enter 0 for start: ");
+            hour = int.Parse(Console.ReadLine());
+            hour = 1;
+
+            while (hour > 0)
+            {
+                if (hour > max)
+                {
+                    max = hour;
+                    Console.WriteLine("\nEnter 0 for Exit");
+                    Console.Write("Enter hour: ");
+                    hour = int.Parse(Console.ReadLine());
+                }
+
+            }
+            Console.WriteLine("max: {0}", max);
+
+            Console.ReadKey();
+        }
+    }
+}
